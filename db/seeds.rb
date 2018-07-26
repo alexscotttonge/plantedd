e7e52899ae3646e397cb433484be0cb7daf8eb92
Alex <alex@scotttonge.com>
2018-07-26 13:51:41 +0100

    Added seeding for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create!(name: "ABIES nordmanniana",
+  description: "Caucasian Fir A large ornamental tree that holds
+                its rich green needles longer than any other Fir,
+                on tiered branches sweeping downwards. Produces
+                large brown cones, prefers deep moist soil. Dense growing.
+                Height 35-50 metres.",
+  image_url: 'Abies_nordmanniana.jpg',
+  price: 100.00)
+# . . .
+
+Product.create!(name: "KNAUTIA macedonica",
+  description: "A variegated leaved variety, the leaves are light green
+                with a broad cream margin. The flowers are double, and are
+                richruby red, and appear from mid summer onwards. Height 45cm.",
+  image_url: 'Knautia_macedonia.jpg',
+  price: 9.25)
+# . . .
+
+Product.create!(name: "DICKSONIA antarctica",
+  description: "Tree Fern Develops an attractive trunk after a few years.
+                Best in moist soil in a sheltered position. Protect by wrapping
+                in winter, or if in a pot it can be moved to frost free position.",
+  image_url: 'Dicksonia_antarctica.jpg',
+  price: 130.00)
